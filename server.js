16394955623d6542c0a49e83e8fc7f556acb5c33
rjerome <rjerome@architech.ca>
2018-07-03 13:54:35 -0400

    More skeleton code

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 var express = require('express');
 
 var app = express();
 
 app.get('/', function(req, res) {
-  res.send({
-    "Output": "Hello World!"
-  });
-});
-
-app.post('/', function(req, res) {
-  res.send({
-    "Output": "Hello World!"
-  });
+  res.send(
+    "Server up!"
+  );
 });
 
 app.listen(port);
